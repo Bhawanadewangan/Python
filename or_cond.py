7f47c7984b75c51defac647ebe2ec0a03ddf7706
Bhawanadewangan <115826888+Bhawanadewangan@users.noreply.github.com>
2022-11-08 18:16:51 +0100

    Add files via upload

---
+++ or_cond.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  8 15:55:32 2022
+
+@author: bhaw
+"""
+
+#or condition
+a = 200 
+b = 33 
+c = 500 
+if a > b or a > c: 
+    print("At least one of the conditions is True")
